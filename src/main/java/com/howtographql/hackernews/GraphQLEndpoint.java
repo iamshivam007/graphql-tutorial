4e3e02732061363ca7e5590d95679ff13b1e4259
iamshivam007 <jindalshivam65@gmail.com>
2022-05-29 23:48:21 +0530

    initial commit

---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,28 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+
+import com.howtographql.hackernews.Query;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        MongoDatabase mongoDatabase = new MongoClient().getDatabase("hackernews");
+        LinkRepository linkRepository = new LinkRepository(mongoDatabase.getCollection("links"));
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
---
+++ src/main/java/com/howtographql/hackernews/LinkRepository.java
@@ -0,0 +1,45 @@
+package com.howtographql.hackernews;
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkRepository {
+
+    private final MongoCollection<Document> links;
+
+    public LinkRepository(MongoCollection<Document> links) {
+        this.links = links;
+    }
+
+    public Link findById(String id) {
+        Document document = links.find().first();
+        return link(document);
+    }
+
+    public List<Link> getAllLinks() {
+        List<Link> allLinks = new ArrayList<>();
+        for(Document document: links.find()) {
+            allLinks.add(link(document));
+        }
+        return allLinks;
+    }
+
+    public void saveLink(Link link) {
+        Document document = new Document();
+        document.put("url", link.getUrl());
+        document.put("description", link.getDescription());
+        links.insertOne(document);
+    }
+
+    private Link link(Document document) {
+        return new Link(
+                document.get("_id").toString(),
+                document.getString("url"),
+                document.getString("description"));
+    }
+}
